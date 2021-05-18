d5354043d11164f953d08cbaa218b0e618b433fa
saharmehrpour <smehrpou@masonlive.gmu.edu>
2021-05-18 15:10:25 -0400

    fixed issues related to expr in RulePad

--- src/ui/RulePad/rulePad.js
+++ src/ui/RulePad/rulePad.js
@@ -1111,10 +1111,11 @@ class RulePad extends Component {
                     let resultXPath = this.traverseReceivedXml(receivedMessages[j]["xmlText"], sentMessages[index]);
                     // replace all occurrences of textAndXPath.originalText
 
-                    let regQ = new RegExp(sentMessages[j]["lookFor"], "g");
-                    quantifierXPath = quantifierXPath.replace(regQ, resultXPath);
-                    let regC = new RegExp(sentMessages[j]["lookFor"], "g");
-                    constraintXPath = constraintXPath.replace(regC, resultXPath);
+                    while (quantifierXPath.indexOf(sentMessages[j]["lookFor"]) !== -1)
+                        quantifierXPath = quantifierXPath.replace(sentMessages[j]["lookFor"], resultXPath);
+                    while (constraintXPath.indexOf(sentMessages[j]["lookFor"]) !== -1)
+                        constraintXPath = constraintXPath.replace(sentMessages[j]["lookFor"], resultXPath);
+
 
                     matchedIndices.sent.push(index);
                     matchedIndices.received.push(j);
